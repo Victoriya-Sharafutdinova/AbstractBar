ac500c85b7877f5d57cf99c14e7345d59c64958e
Victoriya-Sharafutdinova <43270403+Victoriya-Sharafutdinova@users.noreply.github.com>
2019-05-30 15:44:29 +0400

    Update MainController.cs

--- AbstractGarmentFactoryRestApi/Controllers/MainController.cs
+++ AbstractGarmentFactoryRestApi/Controllers/MainController.cs
@@ -39,18 +39,7 @@ namespace AbstractGarmentFactoryRestApi.Controllers
         {
             _service.CreateIndent(model);
         }
-        /*
-        [HttpPost]
-        public void TakeIndentInWork(IndentBindingModel model)
-        {
-            _service.TakeIndentInWork(model);
-        }
-
-        [HttpPost]
-        public void FinishIndent(IndentBindingModel model)
-        {
-            _service.FinishIndent(model);
-        }*/
+      
 
         [HttpPost]
         public void PayIndent(IndentBindingModel model)
